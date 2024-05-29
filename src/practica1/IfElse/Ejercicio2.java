e0229c063fe01aea79cc75f38eac9a4d83a089ed
dpicuasi <danyeledg@gmail.com>
2024-05-28 22:01:09 -0500

    Ajustes en clase

--- src/practica1/IfElse/Ejercicio2.java
+++ src/practica1/IfElse/Ejercicio2.java
@@ -10,18 +10,19 @@ public class Ejercicio2 {
      public void main() {
          String nombre="pepe";
          String contrasenia ="123";
-         String User, Clave;
+         String user;
+         String clave;
          
          System.out.println("* (1.1).- IF-ELSE 1 ");
          Scanner sc = new Scanner(System.in);
          System.out.println("Ingrese el Usuario:");
-         User = sc.nextLine();
+         user = sc.nextLine();
          
          System.out.println("Ingrese la Clave:");
-         Clave = sc.nextLine();
+         clave = sc.nextLine();
          
-         if(nombre.equals(User) && contrasenia.equals(Clave)){
-             System.out.println("Bienvenido "+User);
+         if(nombre.equals(user) && contrasenia.equals(clave)){
+             System.out.println("Bienvenido "+user);
          }else{
              System.out.println("Usuario o clave incorrectos");
          }
